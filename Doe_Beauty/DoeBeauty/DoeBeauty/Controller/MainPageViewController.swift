f0f83d54b10737e3b4fd5c87d6c8285bfdc1c2fe
Florian Peyrony <peyronyflorian.pro@gmail.com>
2023-08-16 15:09:48 +0200

    Add files via upload

---
+++ Doe_Beauty/DoeBeauty/DoeBeauty/Controller/MainPageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainPageViewController.swift
+//  DoeBeauty
+//
+//  Created by Florian Peyrony on 12/07/2023.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+    
+    @IBOutlet weak var leadingHamburgerView: NSLayoutConstraint!
+    @IBOutlet weak var backViewHamburgerView: UIView!
+    
+    @IBOutlet weak var hamburgerView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //backViewHamburgerView.isHidden = true
+        leadingHamburgerView.constant = -300
+        
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func pressedHamburgerButton(_ sender: Any) {
+        //self.backViewHamburgerView.isHidden = false
+        leadingHamburgerView.constant = 0
+    }
+}
---
+++ Doe_Beauty/README.md
@@ -0,0 +1,2 @@
+# Doe_Beauty
+App for nail technician
